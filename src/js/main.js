583f4e2b765e33b18fd4a8b98718ad36faaa703c
Eli Murdock <elimurdock85@gmail.com>
2025-09-21 00:44:32 -0600

    scrolling pictures

--- src/js/main.js
+++ src/js/main.js
@@ -6,11 +6,28 @@ const disclaimer = document.querySelector(".disclaimer > a");
 disclaimer.href = parkData.url;
 disclaimer.innerHTML = parkData.fullName;
 
+
 function parkInfoTemplate(info) {
-  return `<a href="/" class="hero-banner__title">${info.name}</a>
-  <p class="hero-banner__subtitle">
+  return `<a href="/" class="cover-picture-title">${info.name}</a>
+  <p class="cover-picture-subtitle">
     <span>${info.designation}</span>
     <span>${info.states}</span>
   </p>`;
 }
 
+const coverPictureText = document.querySelector(".cover-picture-text");
+coverPictureText.innerHTML = parkInfoTemplate(parkData);
+
+document.title = parkData.name;
+
+const coverPicture = document.querySelector(".cover-picture > img");
+const images = parkData.images;
+let currentImage = 0;
+
+function changeCoverPicture() {
+    coverPicture.src = images[currentImage].url;
+    currentImage = (currentImage + 1) % images.length;
+}
+
+changeCoverPicture();
+setInterval(changeCoverPicture, 10000);
